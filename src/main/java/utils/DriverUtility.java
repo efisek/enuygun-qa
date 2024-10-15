54db20cf073a789977870c000aff97ef638c5706
efisek <fisekemre@gmail.com>
2024-10-15 23:44:29 +0300

    screenshot feature added under driverutility

--- src/main/java/utils/DriverUtility.java
+++ src/main/java/utils/DriverUtility.java
@@ -1,4 +1,37 @@
 package utils;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DriverUtility {
+
+    public static void takeScreenshot(WebDriver driver, String fileName) {
+        try {
+            //Not to overwrite the screenshots
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+            LocalDateTime now = LocalDateTime.now();
+            String dateTimeString = now.format(formatter);
+            String fullFileName = fileName + "_" + dateTimeString + ".png";
+
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+            Path destinationPath = Paths.get("test-output/screenshots", fullFileName);
+
+            Files.createDirectories(destinationPath.getParent());
+            Files.copy(srcFile.toPath(), destinationPath);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
