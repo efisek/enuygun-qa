00e077178cf2c7fa53256783b5ba9e0b8f3bc7ef
efisek <fisekemre@gmail.com>
2024-10-16 22:09:49 +0300

    elements created for searchpage

--- src/main/java/pages/FlightTicketSearchPage.java
+++ src/main/java/pages/FlightTicketSearchPage.java
@@ -1,5 +1,32 @@
 package pages;
 
-public class FlightTicketSearchPage {
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FlightTicketSearchPage extends BasePage {
+
+    public FlightTicketSearchPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    //Elements
+    @FindBy(xpath = "//div[@class='ctx-filter-departure-return-time card-header']")
+    public WebElement filterDepartureReturnTime;
+
+    @FindBy(xpath = "//p[@class='search__filter_departure-noon active']")
+    public WebElement filterDepartureTimeNoon;
+
+    @FindBy(xpath = "//div[@class='ctx-filter-airline card-header']")
+    public WebElement filterAirlines;
+
+    @FindBy(xpath = "//div[@data-testid='departureDepartureTimeSlider']")
+    public WebElement departureTimeSlider;
+
+
+
 
 }
